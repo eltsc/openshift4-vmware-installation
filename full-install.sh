fbddf363b649fbbe55ef0cb1272a634efcc67afd
eltsc <eladts.13@gmail.com>
2020-02-18 14:07:38 +0200

    upgraded stolon

--- full-install.sh
+++ full-install.sh
@@ -2,23 +2,37 @@
 
 set -x
 
-read -e -p "Enter Your cluster name: " -i "vmopen" cluster
+# read -e -p "Enter Your cluster name: " -i "vmopen" cluster
 
-cluster="${cluster:-vmopen}"
+# cluster="${cluster:-vmopen}"
 
-oc login -u kubeadmin https://api.$cluster.bynet.dev:6443
+# oc login -u kubeadmin https://api.$cluster.bynet.dev:6443
 
-oc create -f sc-slave.yaml
+# oc create -f sc-slave.yaml
 
-read -e -p "Enter Your postgresql Namespace: " -i "postgresql" postgresNamespace
+# read -e -p "Enter Your postgresql Namespace: " -i "postgresql" postgresNamespace
 
-postgresNamespace="${postgresNamespace:-postgresql}"
+# postgresNamespace="${postgresNamespace:-postgresql}"
 
-oc new-project $postgresNamespace
+# oc new-project $postgresNamespace
 
-helm upgrade --install stolon ./stolon  
+
+# oc create secret generic pg-su --from-literal=username='postgres' --from-literal=password='password'
+
+# oc create secret generic pg-repl --from-literal=username='repl_username' --from-literal=password='repl_password'
+
+# helm upgrade --install $postgresNamespace ./stolon  
+
+# COMMAND=$(kubectl get pod $postgresNamespace-stolon-keeper-0 -o 'jsonpath={.status.phase}')
+# until [[ $COMMAND == "Running%" ]]
+# do
+#     echo 'waiting for pod to be ready'
+#     sleep 2
+# done
 
 PROXYIP=$(kubectl get services | fgrep proxy | grep -oP '\d+\.\d+\.\d+\.\d+')
+
+# oc exec -it $postgresNamespace-stolon-keeper-0 -- psql --host $PROXYIP --port 5432 --username postgres -W -c 'CREATE DATABASE gitlabhq_production OWNER postgres;' 
 
 read -e -p "Enter Your gitlab Namespace: " -i "gitlab" gitlabNamespace
 
--- tests.sh
+++ tests.sh
@@ -21,11 +21,42 @@ echo hey
 #done
 #echo hey2
 
-echo "Do you wish to install this program?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes) echo "yes"; break;;
-        No) echo "no";break;;
-    esac
+# echo "Do you wish to install this program?"
+# select yn in "Yes" "No"; do
+#     case $yn in
+#         Yes) echo "yes"; break;;
+#         No) echo "no";break;;
+#     esac
+# done
+# echo hey2
+# read -e -p "Enter Your cluster name: " -i "vmopen" cluster
+
+# cluster="${cluster:-vmopen}"
+
+# oc login -u kubeadmin https://api.$cluster.bynet.dev:6443
+
+# oc create -f sc-slave.yaml
+
+read -e -p "Enter Your postgresql Namespace: " -i "postgresql" postgresNamespace
+
+postgresNamespace="${postgresNamespace:-postgresql}"
+
+# oc new-project $postgresNamespace
+
+# oc create secret generic pg-su --from-literal=username='postgres' --from-literal=password='password'
+
+# oc create secret generic pg-repl --from-literal=username='repl_username' --from-literal=password='repl_password'
+
+# helm upgrade --install $postgresNamespace ./stolon  
+
+COMMAND=$(kubectl get pod $postgresNamespace-stolon-keeper-0 -o 'jsonpath={.status.phase}')
+until [[ $COMMAND == "Running" ]]
+do
+    echo 'waiting for pod to be ready'
+    sleep 2
 done
-echo hey2
+
+PROXYIP=$(kubectl get services | fgrep proxy | grep -oP '\d+\.\d+\.\d+\.\d+')
+
+oc exec -it $postgresNamespace-stolon-keeper-0 -- psql --host $PROXYIP --port 5432 --username postgres -W -c 'CREATE DATABASE gitlabhq_production OWNER postgres;' 
+
